70c98ecb95fb948bc03082eb83d1299c8f9b5daa
Piet <piet.jacobs@student.hogent.be>
2019-12-23 15:44:33 +0100

    Add Unblock-File for Github retrieval

--- scripts/WIN-DC2/Step1.ps1
+++ scripts/WIN-DC2/Step1.ps1
@@ -3,5 +3,10 @@
 # -------------------------------------------------------------------------
 $hostname = "WIN-DC2"
 Rename-Computer -ComputerName $env:COMPUTERNAME -newName $hostname -Force
+#Unblock scripts that were downloaded from internet (Github repo)
+Unblock-File -Path C:\scripts\Step1.ps1
+Unblock-File -Path C:\scripts\Step2.ps1
+Unblock-File -Path C:\scripts\Step3.ps1
+Unblock-File -Path C:\scripts\Step4.ps1
 Set-ItemProperty $RunOnceKey "NextRun" "C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -ExecutionPolicy Unrestricted -File C:\scripts\Step2.ps1"
 Restart-Computer
--- scripts/WIN-EXC-SHP/Step1.ps1
+++ scripts/WIN-EXC-SHP/Step1.ps1
@@ -3,6 +3,12 @@
 # -------------------------------------------------------------------------
 $hostname = "WIN-EXC-SHP"
 Rename-Computer -ComputerName $env:COMPUTERNAME -newName $hostname -Force
+#Unblock scripts that were downloaded from internet (Github repo)
+Unblock-File -Path C:\scripts\Step1.ps1
+Unblock-File -Path C:\scripts\Step2.ps1
+Unblock-File -Path C:\scripts\Step3.ps1
+Unblock-File -Path C:\scripts\Step4.ps1
+Unblock-File -Path C:\scripts\Step5.ps1
 Set-ItemProperty $RunOnceKey "NextRun" "C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -ExecutionPolicy Unrestricted -File C:\scripts\Step2.ps1"
 Restart-Computer
 
