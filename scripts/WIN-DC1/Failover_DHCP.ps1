341426b8d6375f7b1ecad1a86ce7975af187e8c3
Piet Jacobs <piet.jacobs@student.hogent.be>
2019-12-27 19:45:12 +0100

    fix DHCP Failover

--- scripts/WIN-DC1/Failover_DHCP.ps1
+++ scripts/WIN-DC1/Failover_DHCP.ps1
@@ -2,4 +2,4 @@
 # Failover DHCP
 # -------------------------------------------------------------------------
 Set-ExecutionPolicy -ExecutionPolicy ByPass
-Add-DhcpServerv4Failover -ComputerName WIN-DC1.piet.periode1 -Name win-dc1.piet.periode1-win-dc2 -PartnerServer WIN-DC2.piet.periode1 -ScopeId 192.168.100.0 -Force
+Add-DhcpServerv4Failover -ComputerName WIN-DC1 -Name win-dc1.piet.periode1-win-dc2_loadbalance -LoadBalancePercent 50 -MaxClientLeadTime 1:00:00 -StateSwitchInterval 00:45:00 -PartnerServer WIN-DC2 -ScopeId 192.168.100.0 -SharedSecret "Password123"
