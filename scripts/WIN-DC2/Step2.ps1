ae6fdc71a92888ba24c1a20ba1925c9f438d3cda
Piet Jacobs <piet.jacobs@student.hogent.be>
2019-12-27 18:53:35 +0100

    fix DC2 for DomainController promotion

--- scripts/WIN-DC2/Step2.ps1
+++ scripts/WIN-DC2/Step2.ps1
@@ -7,14 +7,15 @@ New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress $ip -PrefixLength 24 -Def
 Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses $gw, $ip
 
 # -------------------------------------------------------------------------
-# Join existing Domain
+# Promote to Domain Controller
 # -------------------------------------------------------------------------
-
 $domainname = "piet.periode1"
-$username = "$domainname\Administrator"
+$username = "PIET\Administrator"
+$credpassword = ConvertTo-SecureString "adminPass123" -AsPlainText -Force
 $password = "P@ss123" | ConvertTo-SecureString -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential($username, $password)
-Add-Computer -DomainName $domainname -Credential $credential
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $credpassword
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools 
+Install-ADDSDomainController -DomainName $domainname -Credential $credential -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$True -SysvolPath "C:\Windows\SYSVOL" -SafeModeAdministratorPassword:($password) -Force:$true 
 $RunOnceKey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
 Set-ItemProperty $RunOnceKey "NextRun" "C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -ExecutionPolicy Unrestricted -File Z:\Step3.ps1"
 Restart-computer
--- scripts/WIN-DC2/Step3.ps1
+++ scripts/WIN-DC2/Step3.ps1
@@ -1,10 +1,11 @@
+﻿# -------------------------------------------------------------------------
+# Configure DNS
 # -------------------------------------------------------------------------
-# Install Domain Controller (Perform on Server Core)
+$Forwarder = "8.8.8.8"
+Add-DnsServerForwarder -IPAddress $Forwarder -PassThru
+
 # -------------------------------------------------------------------------
-$domainname = "piet.periode1"
-$password = "P@ss123" | ConvertTo-SecureString -AsPlainText -Force
-Install-WindowsFeature AD-Domain-Services -IncludeManagementTools 
-Install-ADDSDomainController -DomainName $domainname -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$True -SysvolPath "C:\Windows\SYSVOL" -SafeModeAdministratorPassword:($password) -Force:$true 
-$RunOnceKey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
-Set-ItemProperty $RunOnceKey "NextRun" "C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -ExecutionPolicy Unrestricted -File Z:\Step4.ps1"
-Restart-computer
+# Install DHCP
+# -------------------------------------------------------------------------
+Install-WindowsFeature -Name 'DHCP' -IncludeManagementTools;
+
